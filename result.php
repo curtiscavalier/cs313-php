22a63b288ebf868eb7505eb08745db96d341c3db
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-05-09 02:28:57 -0600

    fixed assignments

--- result.php
+++ result.php
@@ -8,8 +8,6 @@
 
     <body>
     <?php
-    include "libchart/libchart/classes/libchart.php";
-    $chart = new VerticalBarChart(500, 250);
     $happy = $_POST['happy'];
     $hunger = $_POST['hunger'];
     $car = $_POST['car'];
@@ -20,18 +18,18 @@ while(!feof($result)){
    $array[$var] = fgets($result);
    $var ++;
 }
+fclose($result);
 $array[0] = ($array[0]+$happy);
 $array[1] = ($array[1] + $hunger);
 $array[2] = ($array[2] + $car);
 $array[3] = ($array[3] +$student);
-$dataSet = new XYDataSet();
-$dataSet->addPoint(new Point("happy", $array[0]));
-$dataSet->addPoint(new Point("hunger", $array[1]));
-$dataSet->addPoint(new Point("car", $array[2]));
-$dataSet->addPoint(new Point("student", $array[3]));
-$chart->setDataSet($dataSet);
-$chart->setTitle("Monthly usage for www.example.com");
-$chart->render("generated/demo1.png");
+echo "<center>Result</center>";
+echo "<br/><center>People who are happy: ". $array[0] ."</center>";
+echo "<br/><center>People who are happy: ". $array[1] ."</center>";
+echo "<br/><center>People who are happy: ". $array[2] ."</center>";
+echo "<br/><center>People who are happy: ". $array[3] ."</center>";
+$file = fopen("result.txt","w") or die("can't open fine");;
+fwrite($file,$array[0] + "/");
 
 ?>
 </body>
