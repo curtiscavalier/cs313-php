4df2f02e2fafdec62a6ff3e34ec7153e58aa3763
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-05-23 18:31:58 -0600

    new

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-$con=mysqli_connect("http://php-curtiscavalier.rhcloud.com/","adminSvJPw4q","278ZDqaQnCMU");
-	mysqli_select_db($con, "skills");
+	$con=mysqli_connect("www.secretvoice1.com","secretvo_11","c12345");
+	mysqli_select_db($con, "Sills");
 	if(!$con){
 	die('Could not connect: ' .mysql_error());
 	}
