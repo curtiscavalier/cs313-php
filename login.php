9253d9572924547ac61aa6d003e9d8a5a4a43d40
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-05-23 18:51:25 -0600

    new

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 	$con=mysqli_connect("www.secretvoice1.com","secretvo_11","c12345");
-	mysqli_select_db($con, "Sills");
+	mysqli_select_db($con, "Skills");
 	if(!$con){
 	die('Could not connect: ' .mysql_error());
 	}
