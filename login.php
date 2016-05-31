c93b1614447d2131b26c361d74b5e6baa4fd4556
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-05-31 00:22:14 -0600

    new

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 	$con=mysqli_connect("www.secretvoice1.com","secretvo_11","c12345");
 	mysqli_select_db($con, "secretvo_11");
+	echo "hi";
 	if(!$con){
 	die('Could not connect: ' .mysql_error());
 	}
--- week04.php
+++ week04.php
@@ -3,19 +3,110 @@
 <head>
 <meta charset="UTF-8">
 <title>Week04</title>
+<link rel="stylesheet" type = "text/css" href ="week04.php"/>
 </head>
-<p>Below are my skills as a software engineer</p>
+<p>My family</p>
+
 <?php
+header('Content-type: text/css; charset:UTF-8');
 require('login.php');
 
 
-	$statement = mysqli_prepare($con,"SELECT * FROM Skills");
+.tree ul {
+	padding-top: 20px; position: relative;
+	
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+}
+
+.tree li {
+	float: left; text-align: center;
+	list-style-type: none;
+	position: relative;
+	padding: 20px 5px 0 5px;
+	
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+}
+
+.tree li::before, .tree li::after{
+	content: '';
+	position: absolute; top: 0; right: 50%;
+	border-top: 1px solid #ccc;
+	width: 50%; height: 20px;
+}
+.tree li::after{
+	right: auto; left: 50%;
+	border-left: 1px solid #ccc;
+}
+
+.tree li:only-child::after, .tree li:only-child::before {
+	display: none;
+}
+
+.tree li:only-child{ padding-top: 0;}
+
+.tree li:first-child::before, .tree li:last-child::after{
+	border: 0 none;
+}
+.tree li:last-child::before{
+	border-right: 1px solid #ccc;
+	border-radius: 0 5px 0 0;
+	-webkit-border-radius: 0 5px 0 0;
+	-moz-border-radius: 0 5px 0 0;
+}
+.tree li:first-child::after{
+	border-radius: 5px 0 0 0;
+	-webkit-border-radius: 5px 0 0 0;
+	-moz-border-radius: 5px 0 0 0;
+}
+
+.tree ul ul::before{
+	content: '';
+	position: absolute; top: 0; left: 50%;
+	border-left: 1px solid #ccc;
+	width: 0; height: 20px;
+}
+
+.tree li a{
+	border: 1px solid #ccc;
+	padding: 5px 10px;
+	text-decoration: none;
+	color: #666;
+	font-family: arial, verdana, tahoma;
+	font-size: 11px;
+	display: inline-block;
+	
+	border-radius: 5px;
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+}
+
+
+.tree li a:hover, .tree li a:hover+ul li a {
+	background: #c8e4f8; color: #000; border: 1px solid #94a0b4;
+}
+
+.tree li a:hover+ul li::after, 
+.tree li a:hover+ul li::before, 
+.tree li a:hover+ul::before, 
+.tree li a:hover+ul ul::before{
+	border-color:  #94a0b4;
+}
+
+	/*$statement = mysqli_prepare($con,"SELECT * FROM family");
 	mysqli_stmt_bind_param($statement);
 	mysqli_stmt_execute($statement);
 	
 	mysqli_stmt_store_result($statement);
 	
-	mysqli_stmt_bind_result($statement,$id,$Skills);
+	mysqli_stmt_bind_result($statement,$id,);
 	$profile = array();
 	while (mysqli_stmt_fetch($statement)) {
 		echo $id." : ";
@@ -25,9 +116,47 @@ require('login.php');
 	
 	}
 	mysqli_stmt_close($statement);
-	mysqli_close($con);
+	mysqli_close($con);*/
 
 ?>
 <body>
+<div class="tree">
+	<ul>
+		<li>
+			<a href="#">Parent</a>
+			<ul>
+				<li>
+					<a href="#">Child</a>
+					<ul>
+						<li>
+							<a href="#">Grand Child</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Child</a>
+					<ul>
+						<li><a href="#">Grand Child</a></li>
+						<li>
+							<a href="#">Grand Child</a>
+							<ul>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+							</ul>
+						</li>
+						<li><a href="#">Grand Child</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
 </body>
 </html>
