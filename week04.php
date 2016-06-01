8f1dfc99df51795b065b7fc440ca93dbe1b08295
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-06-01 16:43:57 -0600

    new

--- week04.php
+++ week04.php
@@ -4,6 +4,10 @@
 <title>Week04</title>
 <link rel="stylesheet" type="text/css" href="style.php">
 </head>
+
+
+<body>
+
 <p>My family</p>
 <?php
 require('login.php');
@@ -95,47 +99,6 @@ require('login.php');
 	echo "</div>";
 
 ?>
-
-<body>
-
-<div class="tree">
-	<ul>
-		<li>
-			<a href="#">Parent</a>
-			<ul>
-				<li>
-					<a href="#">Child</a>
-					<ul>
-						<li>
-							<a href="#">Grand Child</a>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">Child</a>
-					<ul>
-						<li><a href="#">Grand Child</a></li>
-						<li>
-							<a href="#">Grand Child</a>
-							<ul>
-								<li>
-									<a href="#">Great Grand Child</a>
-								</li>
-								<li>
-									<a href="#">Great Grand Child</a>
-								</li>
-								<li>
-									<a href="#">Great Grand Child</a>
-								</li>
-							</ul>
-						</li>
-						<li><a href="#">Grand Child</a></li>
-					</ul>
-				</li>
-			</ul>
-		</li>
-	</ul>
-</div>
 
 </body>
 </html>
