f791d28c6ac8b3a3f8b140c2976966b9ca3d9705
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-06-02 11:04:34 -0600

    new

--- edit.php
+++ edit.php
@@ -7,10 +7,21 @@
 
 <body>
 <?php
+	require('login.php');
 	if(isset($_GET["data"]))
 	{
 		$data = $_GET["data"];
 	}
+	
 ?>
+<form action="action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="<?php $gn[$data]?>">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="<?php $fn[$data]?>">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
 </body>
 </html>
