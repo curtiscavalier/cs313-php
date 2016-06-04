823c653a0bcf97879d7077bc0c3552f53cbb204a
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-06-04 12:05:54 -0600

    new

--- delete.php
+++ delete.php
@@ -11,13 +11,13 @@ if(isset($_POST["person_id"]))
 	{
 		$data = $_POST["person_id"];
 	}
-	
+	echo $data;
 	$con=mysqli_connect("www.secretvoice1.com","secretvo_11","c12345");
 		mysqli_select_db($con, "secretvo_11");
 		if(!$con){
 		die('Could not connect: ' .mysql_error());
 		}
-		$sql = "DELETE FROM family WHERE person_id = ".$data;
+		$sql = "DELETE FROM family WHERE person_id = '".$data."'";
 		if ($conn->query($sql) === TRUE) {
     echo "Record deleted successfully";
 } else {
