f33fae13544f473c7126e135711d9992de78108b
Curtis Cavalier <curtiscavalier@dsn4.d.skype.net>
2016-06-04 02:29:45 -0600

    new

--- week04.php
+++ week04.php
@@ -45,9 +45,7 @@ echo "<div class ='tree'>";;
 				for ($x = 0; $x < count($temp); $x++) {
 		    		getChild($temp[$x]);
 				}
-				un($i);
-			echo "</li></ul>";
-			
+			}
 			
 		}
 	}
@@ -95,93 +93,7 @@ echo "<div class ='tree'>";;
 		}
 	}
 		echo "</div>";
-	/*$c = (count($id)-1);
-	$count = 0;
-	$final = 0;
-
-	echo "<div class ='tree'><ul>";
 	
-	while($c > 0 ){
-		if(empty($gn[$c])){
-			break;
-		}
-	
-		if(($c-1)!=0){
-			if($gen[$c] == $gen[$c-1]){
-				while($gen[$c] == $gen[$c-1]){
-					echo "<li><a href ='edit.php?data=".$c."'>".($c+1)." ".$gn[$c].$fn[$c]."</a>";
-					if(!empty($re[$c])){
-						$temp= explode(" ",$re[$c]);
-						echo "<ul>";
-						for ($x = 0; $x < count($temp); $x++) {
-	    					getChild($x);
-						}
-					}
-					un($c);
-					$c--;
-					echo "</li></ul>";
-					if($c == -1){
-						break;
-					}
-				}
-			}
-		}
-	if($c == -1){
-			break;
-	}
-	if(empty($gn[$c])){
-		break;
-	}
-			echo "<li><a href ='edit.php?data=".$c."'>".($c+1)." ".$gn[$c].$fn[$c]."</a>";
-		if(!empty($re[$c])){
-			$temp= explode(" ",$re[$c]);
-			echo "<ul>";
-			for ($x = 0; $x < count($temp); $x++) {
-		   		getChild($x);
-			}
-		}
-		echo "</li></ul>";
-		un($c);
-		$c--;
-	}
-	function getChild($t){
-		global $gn;
-		global $marriage;
-		global $fn;
-		global $id;
-		global $re;
-		global $c;
-		if(empty($gn[$t])){
-			return;
-		}
-			echo "<li><a href ='edit.php?data=".$t."'>".($t+1)." ".$gn[$t].$fn[$t]."</a>";
-		if(!empty($re[$t])){
-			while ($re[$t]) {
-				echo "<ul>";
-				getChild($re[$t]);
-			}
-			echo "</li></ul>";
-		}
-		un($t);
-	
-		
-	}
-	function un($var){
-		global $gn;
-		global $marriage;
-		global $fn;
-		global $id;
-		global $re;
-	unset($gen[$var]);
-	unset($id[$var]);
-	unset($gn[$var]);
-	unset($fn[$var]);
-	unset($re[$var]);
-	unset($marriage[$var]);
-	}	
-	echo "</div>";
-*/
-	}
 ?>
 <?php
 	require('login.php');
