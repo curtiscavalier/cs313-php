a8f898f3b0da3081249c1f0aa3eb7984b5729c93
Curtis Cavalier <curtiscavalier@Curtiss-MacBook-Pro.local>
2016-06-04 11:58:07 -0600

    new

--- chinese.php
+++ chinese.php
@@ -10,43 +10,25 @@
 	{
 		$data = $_POST["person_id"];
 	}
-$con=mysqli_connect("www.secretvoice1.com","secretvo_11","c12345");
-	mysqli_select_db($con, "secretvo_11");
-	if(!$con){
-	die('Could not connect: ' .mysql_error());
-	}
+	$statement = mysqli_prepare($con,"SELECT * FROM person_name WHERE person_id = ".$data);
+		mysqli_stmt_bind_param($statement, "iiss",$person_name_id, $person_id,$family_name,$given_name);
+		mysqli_stmt_execute($statement);
+		
+		mysqli_stmt_store_result($statement);
+		
+		mysqli_stmt_bind_result($statement,$person_name_id, $person_id,$family_name,$given_name);
+		$profile = array();
+		while (mysqli_stmt_fetch($statement)) {
+			$profile["person_id"] = $person_id;
+			$profile["family_name"] = $family_name;
+			$profile["given_name"] = $given_name;
+		}
 	
-
-	if(isset($_POST["person_id"]))
-	{
-		$data = $_POST["person_id"];
-	}
-	require('getRow.php');
-	$statement = mysqli_prepare($con,"SELECT * 
-	FROM person_name
-	JOIN family ON family.person_id = ".$data."
-	WHERE person_name.person_id = family.person_id");
-	mysqli_stmt_bind_param($statement, "issiii",$person_id, $family_name,$given_name,$generation,$relation,$marriage_id);
-	mysqli_stmt_execute($statement);
-	
-	mysqli_stmt_store_result($statement);
-	
-	mysqli_stmt_bind_result($statement,$person_id,$family_name,$given_name,$generation,$relation,$marriage_id);
-	$profile = array();
-while (mysqli_stmt_fetch($statement)) {
-		$profile["person_id"] = $person_id;
-		$profile["family_name"] = $family_name;
-		$profile["given_name"] = $given_name;
-		$profile["gen"] = $generation;
-		$profile["re"] = $relation;
-		$profile["marriage"] = $marriage_id;
-	}
-	
-	mysqli_stmt_close($statement);
-	mysqli_close($con);
+		mysqli_stmt_close($statement);
+		mysqli_close($con);	
 ?>
 <body>
-<form action="update.php" method = "post">
+<form >
 <input type = "hidden" name = "person_id" value = "<?php echo $profile["person_id"] ?>">
   First name:<br>
   <input type="text" name="firstname" value="<?php echo $profile["given_name"]?>">
@@ -54,15 +36,7 @@ while (mysqli_stmt_fetch($statement)) {
   Last name:<br>
   <input type="text" name="lastname" value="<?php echo $profile["family_name"]?>">
   <br>
-  relationship with(number):<br>
-  <input type="text" name="relationship" value = "<?php echo $profile["re"]?>">
-  <br>
-  Generation:<br>
-  <input type="text" name="generation" value= "<?php echo $profile["gen"] ?>">
-  <br>
-  This person is married to:<br>
-  <input type="text" name="marriage" value= "<?php echo $profile["marriage"] ?>">
-  <br><br>
+  
   <input type="submit" value="Submit">
 </form> 
 </body>
